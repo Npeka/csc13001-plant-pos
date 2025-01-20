7c8b4aceb18bc364e25f4a8979f68ea5eed21bd2
Npeka <npkhang22@clc.fitus.edu.vn>
2025-01-20 23:47:10 +0700

    feat: add mysql database, docker compose and authentication service

---
+++ csc13001-plant-pos/InitDb/InitMySqlDb.sql
@@ -0,0 +1,11 @@
+﻿-- Tạo user "crm" với mật khẩu "crm" nếu chưa tồn tại
+CREATE USER IF NOT EXISTS 'plantstore'@'%' IDENTIFIED BY 'plantstore';
+
+-- Tạo database "crm" nếu chưa tồn tại
+CREATE DATABASE IF NOT EXISTS plantstore;
+
+-- Gán quyền sở hữu database "crm" cho user "crm"
+GRANT ALL PRIVILEGES ON plantstore.* TO 'plantstore'@'%';
+
+-- Áp dụng các thay đổi quyền
+FLUSH PRIVILEGES;
---
+++ csc13001-plant-pos/Services/AuthenticationService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using csc13001_plant_pos.Data.Contexts;
+using csc13001_plant_pos.Core.Models;
+
+namespace csc13001_plant_pos.Services;
+public class AuthenticationService
+{
+    private readonly ApplicationDbContext _context;
+
+    public AuthenticationService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> RegisterAsync(string username, string password)
+    {
+        if (await _context.Users.AnyAsync(u => u.Username == username))
+        {
+            return false;
+        }
+
+        var user = new User
+        {
+            Username = username,
+            Password = BCrypt.Net.BCrypt.HashPassword(password),
+            Email = "admin@plantstore.com",
+            IsAdmin = false,
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<User?> LoginAsync(string username, string password)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+        if (user != null && BCrypt.Net.BCrypt.Verify(password, user.Password))
+        {
+            return user;
+        }
+
+        return null;
+    }
+}
