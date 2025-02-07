50b01634cd2a42c336ab0505539d67b66cd20de5
Npeka <npkhang22@clc.fitus.edu.vn>
2025-02-07 21:29:54 +0700

    feat: add order and order detail dto

---
+++ csc13001-plant-pos.Core/DTOs/Orders/OrderDto.cs
@@ -0,0 +1,30 @@
+﻿using csc13001_plant_pos.Core.Models;
+
+namespace csc13001_plant_pos.Core.DTOs.Orders;
+public class OrderDto
+{
+    public int OrderId
+    {
+        get; set;
+    }
+    public string CustomerName
+    {
+        get; set;
+    }
+    public string StaffName
+    {
+        get; set;
+    }
+    public DateTime OrderDate
+    {
+        get; set;
+    }
+    public decimal TotalPrice
+    {
+        get; set;
+    }
+    public string Status
+    {
+        get; set;
+    }
+}
