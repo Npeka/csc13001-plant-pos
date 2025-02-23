f1b7f6263f158b8be4d4073b73777d2ccde3b141
JonasAugust12 <cas.nmtoan@gmail.com>
2025-02-24 01:53:21 +0700

    feat: implement interactive functionality for sale page

--- csc13001-plant-pos/Views/SalePage.xaml.cs
+++ csc13001-plant-pos/Views/SalePage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using csc13001_plant_pos.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace csc13001_plant_pos.Views;
@@ -14,6 +14,48 @@ public sealed partial class SalePage : Page
     public SalePage()
     {
         ViewModel = App.GetService<SaleViewModel>();
+        DataContext = ViewModel;
         InitializeComponent();
+    }
+    private void ProductItem_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+    {
+        if (sender is FrameworkElement element && element.Tag is Product product)
+        {
+            ViewModel.AddToOrderCommand.Execute(product);
+        }
+    }
+    private async void NoteButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        var orderItem = button.DataContext as OrderItem;
+
+        // Create TextBox
+        var textBox = new TextBox
+        {
+            PlaceholderText = "Enter note here...",
+            Text = orderItem.Note,
+            TextWrapping = TextWrapping.Wrap,
+            AcceptsReturn = true,
+            Height = 100,
+            Margin = new Thickness(0, 10, 0, 0)
+        };
+
+        // Create ContentDialog
+        var dialog = new ContentDialog
+        {
+            Title = "Add Note",
+            Content = textBox,
+            PrimaryButtonText = "Save",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Primary,
+            XamlRoot = this.XamlRoot
+        };
+
+        // Show dialog and handle result
+        var result = await dialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            orderItem.Note = textBox.Text;
+        }
     }
 }
