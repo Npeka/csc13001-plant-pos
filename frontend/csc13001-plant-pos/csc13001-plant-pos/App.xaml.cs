d5b1820ea411589918e9489869e9ba851b2ba760
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-29 15:03:08 +0700

    refactor: update frontend

--- frontend/csc13001-plant-pos/csc13001-plant-pos/App.xaml.cs
+++ frontend/csc13001-plant-pos/csc13001-plant-pos/App.xaml.cs
@@ -15,26 +15,54 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.Extensions.Hosting;
+using System.Net.Http;
+using csc13001_plant_pos.Service;
+using Microsoft.Extensions.DependencyInjection;
+using csc13001_plant_pos.ViewModel.Authentication;
 
-namespace csc13001_plant_pos
+namespace csc13001_plant_pos;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public IHost Host
     {
-        public App()
-        {
-            this.InitializeComponent();
-            this.RequestedTheme = ApplicationTheme.Light;
-        }
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
-            m_window = new MainWindow();
-            m_window.Activate();
-        }
-        public MainWindow? GetMainWindow()
-        {
-            return m_window as MainWindow;
-        }
-
-        private Window? m_window;
+        get;
     }
+
+    public App()
+    {
+        this.InitializeComponent();
+        this.RequestedTheme = ApplicationTheme.Light;
+
+        Host = Microsoft.Extensions.Hosting.Host.
+        CreateDefaultBuilder().
+        UseContentRoot(AppContext.BaseDirectory).
+        ConfigureServices((context, services) =>
+        {
+            var baseAddress = context.Configuration.GetSection("ApiSettings:BaseAddress").Value;
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress ?? "http://localhost:8080/api/")
+            };
+            services.AddSingleton(httpClient);
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
+
+            // Views and ViewModels
+            services.AddTransient<LoginViewModel>();
+
+        }).
+        Build();
+    }
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+    public MainWindow? GetMainWindow()
+    {
+        return m_window as MainWindow;
+    }
+
+    private Window? m_window;
 }
