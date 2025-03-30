39b982f788523c20cd5496ad1bf7c39f9343cc76
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-31 03:37:15 +0700

    feat: add api helper message

---
+++ frontend/csc13001-plant-pos/csc13001-plant-pos/DTO/ApiResponseHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace csc13001_plant_pos.DTO
+{
+    public static class ApiResponseHelper
+    {
+        public static string MessageServerError()
+        {
+            return "Server error! Please try again.";
+        }
+    }
+}
