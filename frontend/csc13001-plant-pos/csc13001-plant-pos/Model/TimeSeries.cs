ec20b79ccaef6e1c4baee6c479c08bd62c9100e4
Nhân Nguyễn Trọng <nhannguyentrong355@gmail.com>
2025-04-07 23:32:45 +0700

    add statistic viewmodel

---
+++ frontend/csc13001-plant-pos/csc13001-plant-pos/Model/TimeSeries.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace csc13001_plant_pos.Model
+{
+    public class TimeSeries
+    {
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+        [JsonPropertyName("value")]
+        public int revenue { get; set; }
+    }
+}
