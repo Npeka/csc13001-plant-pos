65d3be187c8503ef1a56e6ae6ed3e2c040317ef2
JonasAugust12 <cas.nmtoan@gmail.com>
2025-04-24 22:11:10 +0700

    feat: add chatbot avatar

---
+++ frontend/csc13001-plant-pos/csc13001-plant-pos/Converter/MessageToProfilePictureConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using csc13001_plant_pos.Model;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace csc13001_plant_pos.Converter
+{
+    public class MessageToProfilePictureConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Message message)
+            {
+                if (message.FromBot)
+                {
+                    return new BitmapImage(new Uri("ms-appx:///Assets/Icon/chatbot.jpg"));
+                }
+                else if (!string.IsNullOrEmpty(message.User?.ImageUrl))
+                {
+                    try
+                    {
+                        return new BitmapImage(new Uri(message.User.ImageUrl));
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
